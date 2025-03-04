ebbe174d727294ad958bcd0907bc82a6cac4404d
mythJoel <mythjesi@gmail.com>
2025-03-04 19:13:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
 const num1Txt = document.getElementById("num1");
 const num2Txt = document.getElementById("num2");
-const resultS = document.getElementById("sum");
-const resultD =  document.getElementById("dif");
-const resultP = document.getElementById("prod");
-const resultQ = document.getElementById("qou");
+const num3Txt = document.getElementById("num3");
+const resultTxt = document.getElementById("sum");
 
-let sum = 0;
-let dif = 0;
-let prod = 0;
-let qou = 0;
-
-
-function calculate() {
-    if(!(num1Txt.value === "" || num2Txt.value === "")) {
-        
-        sum = parseInt(num1Txt.value) + parseInt(num2Txt.value);
-        resultS.innerHTML = sum;
-
-        dif = parseInt(num1Txt.value) - parseInt(num2Txt.value);
-        resultD.innerHTML = dif;
-
-        prod = parseInt(num1Txt.value) * parseInt(num2Txt.value);
-        resultP.innerHTML = prod;
-
-        qou = parseInt(num1Txt.value) / parseInt(num2Txt.value);
-        resultQ.innerHTML = qou;
-    }
-
-
+function concatenate() {
+    resultTxt.innerHTML = num1Txt.value + " " + num2Txt.value + " " + num3Txt.value;
 }
 
 function clearEntries() {
     num1Txt.value = "";
     num2Txt.value = ""
-    resultS.innerHTML = "";
-    resultD.innerHTML = "";
-    resultP.innerHTML = "";
-    resultQ.innerHTML = "";
-    sum = 0;    
-    dif = 0;
-    prod = 0;
-    qou = 0;
-
+    num3Txt.value = ""
+    resultTxt.innerHTML = "";
 }
-
